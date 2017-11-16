ff205539351a926138532aad6250a7e02e013319
jiangzhenghui <jiangzhenghui@jd.com>
2017-11-16 10:35:07 +0800

    添加测试模块示例代码

---
+++ src/com/jd/jr/vip/test/Hello.java
@@ -0,0 +1,11 @@
+package com.jd.jr.vip.test;
+
+/**
+ * 新增test模块，并添加测试代码
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
